46db63eb9f7693a9056470e741f9a13f15f579fa
Flyffmario <flyffmario@laposte.net>
2014-01-27 20:51:25 +0100

    Fixed messages and registrations.

--- src/main/java/net/minekingdom/kickban/KickBan.java
+++ src/main/java/net/minekingdom/kickban/KickBan.java
@@ -1,20 +1,22 @@
 package net.minekingdom.kickban;
 
 import net.md_5.bungee.api.plugin.Plugin;
+import net.minekingdom.kickban.commands.BanCommand;
 import net.minekingdom.kickban.commands.KickCommand;
+import net.minekingdom.kickban.commands.UnbanCommand;
 import net.minekingdom.kickban.manager.PlayerManager;
 
 public class KickBan extends Plugin {
 	
-	private static KickBan instance;
 	private PlayerManager playerManager;
 	
 	@Override
 	public void onEnable() {
-		instance = this;
 		this.playerManager = new PlayerManager();
 		this.getProxy().getPluginManager().registerListener(this, new KickBanListener(this));
 		this.getProxy().getPluginManager().registerCommand(this, new KickCommand());
+		this.getProxy().getPluginManager().registerCommand(this, new BanCommand(this));
+		this.getProxy().getPluginManager().registerCommand(this, new UnbanCommand(this));
 	}
 	
 	@Override
@@ -24,9 +26,5 @@ public class KickBan extends Plugin {
 	
 	public PlayerManager getPlayerManager() {
 		return this.playerManager;
-	}
-	
-	public static KickBan getInstance() {
-		return instance;
 	}
 }
